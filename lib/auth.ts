3fe1a8d16eda7d189617b7f224c95f2537ff2ac6
Hamza2079 <hamzahaikal28@gmail.com>
2026-01-01 01:42:48 +0200

    update token

--- lib/auth.ts
+++ lib/auth.ts
@@ -2,7 +2,8 @@ import { decode } from "next-auth/jwt";
 import { cookies } from "next/headers";
 
 export async function getToken(): Promise<string | undefined> {
-    const encryptedToken = (await cookies()).get('next-auth.session-token')?.value
+    const myCookies = await cookies()
+    const encryptedToken=myCookies.get('next-auth.session-token')?.value || myCookies.get('__Secure-next-auth.session-token')?.value
     const decodedToken = await decode({token:encryptedToken,secret:process.env.AUTH_SECRET!})
     return (decodedToken?.token as string | undefined)
 }
